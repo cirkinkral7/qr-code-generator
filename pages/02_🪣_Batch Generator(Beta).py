4e34cf08dd6bb67292cbab565781c6631b8ae9db
ÇirkinKral <153899341+cirkinkral7@users.noreply.github.com>
2025-10-06 17:06:00 +0300

    Add files via upload

---
+++ pages/02_🪣_Batch Generator(Beta).py
@@ -0,0 +1,33 @@
+import streamlit as st
+from streamlit_lottie import st_lottie
+from utils import *
+
+import pandas as pd
+
+st.set_page_config(
+        page_icon="🤳",
+        page_title = "QR Code Gen",
+        layout="centered",
+        initial_sidebar_state="expanded",
+        menu_items={
+        'Get Help': 'https://github.com/cirkinkral7/qrcode',
+        'Report a bug': "https://github.com/cirkinkral7/qrcode/issues",
+        'About': "## A minimalistic application to generate QR Codes using python"
+        })
+
+st.header("Batch Barcoder Generator")
+hide_footer()
+
+st.markdown("--------Work in Progress------")
+try:
+    uploaded_file = st.file_uploader(label="Upload a csv file",type=["csv"])
+    if uploaded_file is not None:
+        st.snow()
+        # Read the Dataframe
+        data_df = pd.read_csv(uploaded_file)
+        # Display the Dataframe
+        st.markdown("### Input Dataframe")
+        st.dataframe(data_df,height=200)
+        st.button("Generate QR Codes")
+except:
+    st.error("Please upload an appropriate CSV file")
---
+++ pages/03_💡_About.py
@@ -0,0 +1,33 @@
+"""
+About Page
+"""
+import streamlit as st
+from streamlit_lottie import st_lottie
+from utils import *
+
+st.set_page_config(
+        page_icon="🤳",
+        page_title = "QR Code Gen",
+        layout="centered",
+        initial_sidebar_state="expanded",
+        menu_items={
+        'Get Help': 'https://github.com/cirkinkral7/qrcode',
+        'Report a bug': "https://github.com/cirkinkral7/qrcode/issues",
+        'About': "## A minimalistic application to generate QR Codes using python"
+        }
+    )
+hide_footer()
+
+st.title(":raising_hand: Frequently Asked Questions")
+
+faq = {
+    "What is the environment needed for this application?": "A: 🐍Python (preferably any version above 3.9)",
+    "Who is the creator?": "@cirkinkral7",
+    "Is this application open-source": "Yes, the appliation and all it's components are open-soruce",
+    "Can we make an executable file for this?": "Yes",
+    "Where can this application be deployed?": "A: 1) Heroku  2) Digital Ocean  3) AWS Beanstalk 4) Streamlit Sharing 5) Render 6)Cyclic"
+}
+for question, answer in faq.items():
+    with st.expander(question):
+        st.write(answer)
+
